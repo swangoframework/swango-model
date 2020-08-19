d90a54032a011c7fe7f4e5ff896153fc7e212686
子旭 苏 <fdream.lhl@foxmail.com>
2020-08-19 11:58:53 +0800

    Make method LocalCacheKey() overloadable and stop using Archer.

--- src/Traits/LocalCacheTrait.php
+++ src/Traits/LocalCacheTrait.php
@@ -17,7 +17,7 @@ trait LocalCacheTrait {
     public static function initCacheTable() {
         self::$local_cache = LocalCache::getInstance(static::class);
     }
-    private static function makeKey(array $where): string {
+    protected static function makeLocalCacheKey(array $where): string {
         $ids = [];
         foreach (self::INDEX as $keyname)
             $ids[] = $where[$keyname];
@@ -26,16 +26,13 @@ trait LocalCacheTrait {
     protected static function loadFromDB($where, bool $for_update = false, bool $force_nornal_DB = false): ?\stdClass {
         if (! isset(self::$local_cache))
             return parent::loadFromDB($where, $for_update, $force_nornal_DB);
-        $key = self::makeKey($where);
+        $key = static::makeLocalCacheKey($where);
         if ($for_update)
             $profile = parent::loadFromDB($where, true, $force_nornal_DB);
         else {
             $profile = self::$local_cache->get($key);
             if (isset($profile))
-                if ($profile['__f__'] === 1)
-                    return null;
-                else
-                    return (object)$profile;
+                return $profile['__f__'] === 1 ? null : (object)$profile;
             $profile = parent::loadFromDB($where, false, $force_nornal_DB);
         }
 
@@ -54,12 +51,11 @@ trait LocalCacheTrait {
             return false;
         $key = implode('`', $index);
         $result = self::$local_cache->del($key);
-        \Swlib\Archer::taskTimerAfter(1, [
+        \Swoole\Timer::after(1000, [
             self::$local_cache,
             'del'
-        ], [
-            $key
-        ]);
+        ], $key);
+
         InternelCmd\DeleteLocalCache::broadcast(static::class, ...$index);
         return $result;
     }
@@ -67,31 +63,23 @@ trait LocalCacheTrait {
         if (! isset(self::$local_cache))
             return false;
         $key = implode('`', $index);
-        \Swlib\Archer::taskTimerAfter(1, [
+        \Swoole\Timer::after(1000, [
             self::$local_cache,
             'del'
-        ], [
-            $key
-        ]);
+        ], $key);
         return self::$local_cache->del($key);
     }
     private function _deleteCache() {
         if (! isset(self::$local_cache))
             return;
 
-        $index = [];
-        foreach (self::INDEX as $keyname)
-            $index[] = $this->where[$keyname];
-
-        $key = implode('`', $index);
+        $key = static::makeLocalCacheKey($this->where);
         self::$local_cache->del($key);
-        \Swlib\Archer::taskTimerAfter(1, [
+        \Swoole\Timer::after(1000, [
             self::$local_cache,
             'del'
-        ], [
-            $key
-        ]);
-        InternelCmd\DeleteLocalCache::broadcast(static::class, ...$index);
+        ], $key);
+        InternelCmd\DeleteLocalCache::broadcast(static::class, $key);
     }
     public function remove(): bool {
         $ret = parent::remove();
