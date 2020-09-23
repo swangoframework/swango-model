cc6d75a9325ff9095b8142d83af42fed7576e308
fdream <fdream@brae.co>
2020-09-23 22:58:28 +0800

    self::$model_name -> static::$model_name

--- src/Exception/ModelNotFoundException.php
+++ src/Exception/ModelNotFoundException.php
@@ -4,7 +4,7 @@ class ModelNotFoundException extends \Exception {
     public static string $model_name;
     private array $index;
     public function __construct(...$index) {
-        parent::__construct(self::$model_name . ' not found');
+        parent::__construct(static::$model_name . ' not found');
         $this->index = $index;
     }
 }
